9eee0e2985316df3a3b8aa5a95027e8f1861fca5
Jani Muhlestein <janimuhlestein@gmail.com>
2020-12-23 20:15:22 -0700

    new router

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
+  HashRouter,
   Switch,
   Route
 } from "react-router-dom";
-import About from './components/About';
-import Nav from './components/Nav';
-import Portfolio from './components/Portfolio';
-import Footer from './components/Footer';
-import ContactForm from './components/Contact';
-import Links from './components/Links';
+import About from './components/About/index.js';
+import Nav from './components/Nav/index.js';
+import Portfolio from './components/Portfolio/index.js';
+import Footer from './components/Footer/index.js';
+import ContactForm from './components/Contact/index.js';
+import Links from './components/Links/index.js';
   
 function App() { 
   return (
-    <Router>
+    <HashRouter basename={process.env.PUBLIC_URL}>
     <div>
       <Nav></Nav>
      <Switch>
-       <Route exact path="/about" component = "about">
+       <Route exact path={"/about"} component = {"about"}>
          <About/>
        </Route>
-       <Route exact path ="/contact" component = "contact">
+       <Route exact path ={"/contact"} component = {"contact"}>
         <ContactForm/>
        </Route>
-       <Route exact path="/portfolio" component = "portfolio">
+       <Route exact path={"/portfolio"} component = {"portfolio"}>
          <Portfolio/>
        </Route>
-       <Route exact path = "/links" component = "links">
+       <Route exact path = {"/links"} component = {"links"}>
          <Links/>
        </Route>
-       <Route path="/">
+       <Route exact path={"/"} component={"about"}>
          <About/>
        </Route>
      </Switch>
       <Footer></Footer>
     </div>
-    </Router>
+    </HashRouter>
   )
 }
 
